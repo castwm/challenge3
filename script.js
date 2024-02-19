0c202c8fa405ac88cd45d25290b5ea3c561ac563
Will <castwm@icloud.com>
2024-02-19 10:33:05 -0500

    password generator

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//generate password
+var passLength = 8;
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var nums = "0123456789"
+var specialChars = "!@#$%^&*()_+-=[]{}|;':,.<>/?";
+var choices = lowerCase + upperCase + nums + specialChars;
+//console.log(choices);
+var password = "";
+for (var i=0; i<passLength; i++){
+  var rand = Math.floor(Math.random() * choices.length);
+  password = password + choices[rand];
+}
+//console.log(password);
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
